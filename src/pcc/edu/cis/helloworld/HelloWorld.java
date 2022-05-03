eb5289468f03b617a21c678743b4516e131f7d97
Erika Earley <erika.earley@pcc.edu>
2022-05-02 19:54:55 -0700

    added author and heading

--- src/pcc/edu/cis/helloworld/HelloWorld.java
+++ src/pcc/edu/cis/helloworld/HelloWorld.java
@@ -2,6 +2,12 @@ package pcc.edu.cis.helloworld;
 
 import java.util.Date;
 
+
+/**
+ * Hello World method
+ * Authors: Cara Tang & Erika Earley
+ * Date: 05/02/22
+ */
 public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hello, World!");
